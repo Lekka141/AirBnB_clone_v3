c9b1c67f9cb70b8cd558d90f165ae56c4f02eae9
Keegan <keeganjoubert@protonmail.com>
2024-05-28 21:52:28 +0000

    Merge

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,15 +1,12 @@
-#!/usr/bin/python3
-"""Blueprint for the API."""
 from flask import Blueprint
-
+"""Blueprint for API"""
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 from api.v1.views.index import *
-from api.v1.views.states import *
-from api.v1.views.cities import *
 from api.v1.views.amenities import *
 from api.v1.views.cities import *
-from api.v1.views.users import *
 from api.v1.views.places import *
-from api.v1.views.places_reviews import *
+from api.v1.views.states import *
+from api.v1.views.users import *
+from api.v1.views.places_review import *
 from api.v1.views.places_amenities import *
