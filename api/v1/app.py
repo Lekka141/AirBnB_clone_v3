f11db47c0f361de0b11e1fcf221a56d9fba7fdc1
Keegan <keeganjoubert@protonmail.com>
2024-05-28 16:51:10 -0700

    PEP8

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,7 +6,6 @@ from api.v1.views import app_views
 from flask import Flask, Blueprint, jsonify, make_response
 from flask_cors import CORS
 
-
 app = Flask(__name__)
 app.register_blueprint(app_views)
 cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
@@ -16,8 +15,10 @@ cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 def after_request(response):
     """Ensure all responses have CORS headers."""
     response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    response.headers.add('Access-Control-Allow-Headers',
+                         'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods',
+                         'GET,PUT,POST,DELETE,OPTIONS')
     return response
 
 
@@ -30,6 +31,7 @@ def teardown_appcontext(code):
 @app.errorhandler(404)
 def page_not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
+
 
 if __name__ == "__main__":
     app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
