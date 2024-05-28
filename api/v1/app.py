fe4e5be9d463d8b656e869c11cbd362e52238dab
Keegan <keeganjoubert@protonmail.com>
2024-05-28 20:29:00 +0000

    App.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,22 +6,17 @@ from api.v1.views import app_views
 from flask import Flask, Blueprint, jsonify, make_response
 from flask_cors import CORS
 
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
 cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
-@app.after_request
-def after_request(response):
-    """Ensure all responses have CORS headers."""
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-    return response
 
 @app.teardown_appcontext
 def teardown_appcontext(code):
     """teardown_appcontext"""
     storage.close()
+
 
 @app.errorhandler(404)
 def page_not_found(error):
